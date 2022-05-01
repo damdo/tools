e43da43f5996bf4595d10b3b1705aaeac1322df0
Damiano Donati <damiano.donati@gmail.com>
2022-05-01 19:34:11 +0100

    add BOOTAA64.EFI, grub.efi, grub.cfg

--- third_party/systemd-248.3-2/systemd.go
+++ third_party/systemd-248.3-2/systemd.go
@@ -5,3 +5,12 @@ import "embed"
 
 //go:embed systemd-bootx64.efi
 var SystemdBootX64 embed.FS
+
+//go:embed BOOTAA64.EFI
+var SystemdBootAArch64 embed.FS
+
+//go:embed grub.efi
+var SystemdBootGrub embed.FS
+
+//go:embed grub.cfg
+var SystemdBootGrubCfg embed.FS
